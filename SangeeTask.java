d94a1416a5d7d29adca06f8d4db8723db08f3416
Reception <sangeethpriyan>
2023-03-23 17:45:04 +0530

    my task code update

---
+++ SangeeTask.java
@@ -0,0 +1,23 @@
+package programs;
+
+public class SangeeTask {
+	
+	public static void main(String[] args) {
+		String str = "alpha123beta123";
+		System.out.println(SangeeTask.reverseOnlyLetters(str));
+	}
+	 public static String reverseOnlyLetters(String S) {
+	        StringBuilder sb = new StringBuilder(S);
+	        for (int i = 0, j = S.length() - 1; i < j;) {
+	            if (!Character.isLetter(sb.charAt(i))) {
+	                ++i;
+	            } else if (!Character.isLetter(sb.charAt(j))) {
+	                --j;
+	            } else {
+	                sb.setCharAt(i, S.charAt(j));
+	                sb.setCharAt(j--, S.charAt(i++));
+	            }
+	        }
+	        return sb.toString();
+	    }
+}
